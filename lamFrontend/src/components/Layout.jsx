4d400632f3115789ece48dcd50718fcdcf90380f
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-07-28 17:50:15 +0600

    changed some style

--- lamFrontend/src/components/Layout.jsx
+++ lamFrontend/src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from './SideBar';
+import Sidebar from './Sidebar';
 
 const Layout = () => {
     return (
         <div className="flex min-h-screen bg-gray-50">
             <Sidebar />
-            <main className="flex-1 p-6">
+            <main className="flex-1 p-2">
                 <Outlet />
             </main>
         </div>

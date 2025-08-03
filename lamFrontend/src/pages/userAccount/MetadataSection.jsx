81ee9eb2889cf455e4bfcb979c4d8cb8d357e9c5
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-08-03 11:20:01 +0600

    chnaged

--- lamFrontend/src/pages/userAccount/MetadataSection.jsx
+++ lamFrontend/src/pages/userAccount/MetadataSection.jsx
@@ -9,7 +9,7 @@ const MetadataSection = ({ formData, handleChange }) => {
         <>
             <SectionTitle>
                 <Info className="w-5 h-5 text-red-600 mr-2" />
-                Submitted by
+                Submission Info
             </SectionTitle>
 
             <div className="bg-white rounded-md border border-red-200 px-6 py-6 grid grid-cols-1 md:grid-cols-3 gap-y-2 md:gap-y-2 gap-x-4 md:gap-x-8 mb-14 shadow">

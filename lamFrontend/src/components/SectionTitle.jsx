f4dceade0eb1d34d4338b4738341841077c7b9f3
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-08-04 15:20:24 +0600

    changed section title

--- lamFrontend/src/components/SectionTitle.jsx
+++ lamFrontend/src/components/SectionTitle.jsx
@@ -1,17 +1,5 @@
 export const SectionTitle = ({ children }) => (
-    <div className="group mb-2 flex items-center gap-2 sm:gap-3 transition-all duration-300">
-        {/* Dash/Line */}
-        <div
-            className="w-[12px] h-[32px] sm:w-[16px] sm:h-[40px] rounded-sm
-                 bg-gradient-to-b from-blue-500 to-blue-700
-                 group-hover:w-[4px] group-hover:rounded-none
-                 group-hover:bg-blue-700
-                 transition-all duration-300 ease-in-out"
-        ></div>
-
-        {/* Icon + Text container */}
-        <div className="flex items-center gap-2 text-gray-700 group-hover:text-blue-700 font-sans font-semibold text-base sm:text-xl">
-            {children}
-        </div>
-    </div>
+    <h2 className="mb-2 flex items-center gap-2 text-gray-800 font-semibold text-lg sm:text-xl transition-colors duration-300 hover:text-blue-700">
+        {children}
+    </h2>
 );

6e80ba34b81ca5e8891267b1bc8abb811c4e323d
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-08-04 15:46:15 +0600

    working on font

--- lamFrontend/src/components/SectionTitle.jsx
+++ lamFrontend/src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 export const SectionTitle = ({ children }) => (
     <h2
-        style={{ fontFamily: 'IDLC' }}
+        style={{ fontFamily: 'IDLCFont' }}
         className="mb-2 flex items-center gap-2 text-gray-800 font-semibold text-lg sm:text-xl transition-colors duration-300 hover:text-blue-700"
     >
         {children}

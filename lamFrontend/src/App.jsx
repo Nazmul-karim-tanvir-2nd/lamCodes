5e1c1f4519af524917a775a4622887f2970c7974
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-08-13 14:02:11 +0600

    updated path

--- lamFrontend/src/App.jsx
+++ lamFrontend/src/App.jsx
@@ -6,9 +6,9 @@ import EmployeeTransfer from './pages/employeeTransfer/EmployeeTransfer.jsx';
 import EmployeeClearance from './pages/employeeClearance/EmployeeClearance.jsx';
 import Layout from './layout/Layout.jsx';
 import LoginPopup from './components/auth/LoginPopup.jsx';
-import AccessReviewDashboard from './pages/ITAdmin/accessReview/AccessReviewDashboard.jsx';
+import AccessReviewDashboard from './pages/ITAdmin/accessReviewDashboard/AccessReviewDashboard.jsx';
 import AdminDashboard from './pages/ITAdmin/AdminDashboard.jsx';
-import AccessReviewDetails from './pages/ITAdmin/accessReview/AccessReviewDetails.jsx';
+import AccessReviewDetails from './pages/ITAdmin/accessReviewDashboard/AccessReviewDetails.jsx';
 
 function App() {
   return (

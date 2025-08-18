6d5b3773b9f18100add47f381853c4596cb205ad
nafisamuhammadd <nafisashuva754@gmail.com>
2025-08-18 11:40:33 +0600

    Update BiometricVerificationStatus.cs

--- LogicalAccessMgmt/Data/Models/BiometricVerificationStatus.cs
+++ LogicalAccessMgmt/Data/Models/BiometricVerificationStatus.cs
@@ -1,7 +1,10 @@
-﻿namespace LogicalAccessMgmt.Data.Models { 
-    public class BiometricVerificationStatus { 
-        public string CIFNo { get; set; } = ""; 
+﻿namespace LogicalAccessMgmt.Data.Models
+{
+    public class BiometricVerificationStatus
+    {
+        public string CIFNo { get; set; } = "";
         public bool NIDVerificationStatus { get; set; } // 0 or 1
         public bool FingerVerificationStatus { get; set; }  //0 or 1 
         public bool FaceVerificationStatus { get; set; } // 0 or 1
     }
+}
